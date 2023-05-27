d7b959b31760e8901060923e59ce8b01cd8f209a
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-05-27 19:50:55 +0300

    Reformat Linter FIle

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,14 +1,13 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": ["eslint:recommended", "prettier"],    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {a
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: { a },
 }
